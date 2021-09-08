88f13a0182bd2e500354af3848921955de5672a6
Juan Vasquez <juan@logicalbricks.com>
2021-09-07 21:36:22 -0500

    Add dark mode by default

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -5,4 +5,25 @@ import "index.scss"
 const componentsContext = require.context("bridgetownComponents", true, /.js$/)
 componentsContext.keys().forEach(componentsContext)
 
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  document.documentElement.classList.add("dark")
+} else {
+  document.documentElement.classList.remove("dark")
+}
+
+// Whenever the user explicitly chooses light mode
+//localStorage.theme = "light"
+
+// Whenever the user explicitly chooses dark mode
+localStorage.theme = "dark"
+console.log("localStorage ", localStorage.getItem("theme"))
+
+// Whenever the user explicitly chooses to respect the OS preference
+//localStorage.removeItem("theme")
+
 console.info("Bridgetown is loaded!")
