bf9492c467be192c508e74c626aba9282c5fc32f
Juan Vasquez <juan@logicalbricks.com>
2021-09-08 00:59:57 -0500

    add dark mode button

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,29 +1,9 @@
 import "gruvbox.css"
 import "index.scss"
+import "dark_mode"
 
 // Import all javascript files from src/_components
 const componentsContext = require.context("bridgetownComponents", true, /.js$/)
 componentsContext.keys().forEach(componentsContext)
-
-// On page load or when changing themes, best to add inline in `head` to avoid FOUC
-if (
-  localStorage.theme === "dark" ||
-  (!("theme" in localStorage) &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches)
-) {
-  document.documentElement.classList.add("dark")
-} else {
-  document.documentElement.classList.remove("dark")
-}
-
-// Whenever the user explicitly chooses light mode
-//localStorage.theme = "light"
-
-// Whenever the user explicitly chooses dark mode
-localStorage.theme = "dark"
-console.log("localStorage ", localStorage.getItem("theme"))
-
-// Whenever the user explicitly chooses to respect the OS preference
-//localStorage.removeItem("theme")
 
 console.info("Bridgetown is loaded!")
