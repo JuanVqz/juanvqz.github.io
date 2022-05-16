f25edfcac2fbc18dffa820f4b0ce0cb06a420ebd
Juan Vásquez <juan@ombulabs.com>
2022-05-15 23:36:14 -0500

    Add bridgetownrb docker config post (#131)

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -3,9 +3,15 @@ import Prism from "prismjs"
 
 import "prismjs/themes/prism-solarizedlight.css"
 
+import "prismjs/components/prism-bash.js"
+import "prismjs/components/prism-docker.js"
+import "prismjs/components/prism-dot.js"
+import "prismjs/components/prism-yaml.js"
+import "prismjs/components/prism-ruby.js"
+
 Prism.highlightAll()
 
 // Import all JavaScript & CSS files from src/_components
-import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}";
+// import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}";
 
 console.info("Bridgetown is loaded!")
